aa16f2a19a3bf2dc661305c3646dd8238509cd23
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-09-02 14:35:23 -0700

    File uploader project inital setup

---
+++ app.js
@@ -0,0 +1,87 @@
+import express from "express";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs";
+import expressSession from 'express-session';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+const __dirname = import.meta.dirname;
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded());
+
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000
+    },
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
+
+app.use(flash());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const rows = await findUsername(username);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Log in failed" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Log in failed" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const rows = await findUserById(id);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+app.use("/", (req, res) => {
+  res.send("Not found");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
